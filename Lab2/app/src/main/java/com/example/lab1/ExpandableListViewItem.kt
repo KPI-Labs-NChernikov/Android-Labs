4c35f12728caac21b8f58709d7e5d5d90d1db209
Chernikov Nikita <14nik20@gmail.com>
2022-10-16 17:33:21 +0300

    Started doing the Lab2

---
+++ Lab2/app/src/main/java/com/example/lab1/ExpandableListViewItem.kt
@@ -0,0 +1,5 @@
+package com.example.lab1
+
+import android.view.View
+
+data class ExpandableListViewItem(val view: View, val position: ExpandableListViewItemPosition)
---
+++ Lab2/app/src/main/java/com/example/lab1/NoScrollExpandableListView.kt
@@ -0,0 +1,27 @@
+package com.example.lab1
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.ExpandableListView
+
+class NoScrollExpandableListView : ExpandableListView {
+
+    constructor(context: Context?) : super(context)
+
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+
+    constructor(context: Context?, attrs: AttributeSet?, defStyle: Int) : super(
+        context,
+        attrs,
+        defStyle
+    )
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        val customHeightMeasureSpec = MeasureSpec.makeMeasureSpec(
+            Int.MAX_VALUE shr 2, MeasureSpec.AT_MOST
+        )
+        super.onMeasure(widthMeasureSpec, customHeightMeasureSpec)
+        val params = layoutParams
+        params.height = measuredHeight
+    }
+}
